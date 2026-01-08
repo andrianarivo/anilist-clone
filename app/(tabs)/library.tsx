9588e08755dda39a7fa106eb5aaae14755ec577c
David <andrianarivo.david@gmail.com>
2026-01-08 15:45:27 +0300

    refactor: Remove unused tab screens for Home, Library, Profile, Search, and Social

--- app/(tabs)/library.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function LibraryScreen() {
-	return (
-		<View className="flex-1 items-center justify-center ">
-			<Text className="text-white text-xl font-bold">Library</Text>
-		</View>
-	);
-}
--- app/(tabs)/profile.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function ProfileScreen() {
-	return (
-		<View className="flex-1 items-center justify-center ">
-			<Text className="text-white text-xl font-bold">Profile</Text>
-		</View>
-	);
-}
