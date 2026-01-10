b16d6f0ac629d9ca3f87d22c7a9758a686605a95
David <andrianarivo.david@gmail.com>
2026-01-10 19:23:49 +0300

    chore: add PostCSS configuration for Tailwind CSS and update TypeScript JSX settings

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,7 @@
 module.exports = (api) => {
   api.cache(true)
   return {
-    presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-      'nativewind/babel',
-    ],
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }]],
     plugins: ['react-native-reanimated/plugin'],
   }
 }
