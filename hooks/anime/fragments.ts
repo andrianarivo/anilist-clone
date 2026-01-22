0ba3a9fa01c3cd792ef49d15f2de51018e1acedd
David <andrianarivo.david@gmail.com>
2026-01-22 10:41:16 +0300

    feat: add AnimeDetailsData fragment and useAnimeDetails hook for fetching anime details

---
+++ hooks/anime/fragments.ts
@@ -0,0 +1,36 @@
+import { graphql } from 'types/gql'
+
+export const AnimeDetailsData = graphql(`
+  fragment AnimeDetailsData on Media {
+    id
+    studios(isMain: true) {
+      nodes {
+        name
+      }
+    }
+    coverImage {
+      extraLarge
+    }
+    startDate {
+      year
+    }
+    popularity
+    averageScore
+    description(asHtml: true)
+    title {
+      userPreferred
+    }
+    characters(role: MAIN) {
+      nodes {
+        id
+        name {
+          userPreferred
+        }
+        gender
+        image {
+          medium
+        }
+      }
+    }
+  }
+`)
---
+++ hooks/anime/use-anime-details.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@apollo/client/react'
+import { graphql } from 'types/gql'
+import type { GetMediaQuery, GetMediaQueryVariables } from 'types/gql/graphql'
+
+const GET_ANIME_DETAILS = graphql(`
+  query GetMedia($id: Int) {
+    Media(id: $id) {
+      ...AnimeDetailsData
+    }
+  }
+`)
+
+export const useAnimeDetails = (id: number) => {
+  return useQuery<GetMediaQuery, GetMediaQueryVariables>(GET_ANIME_DETAILS, {
+    variables: { id },
+    skip: !id,
+  })
+}
